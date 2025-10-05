ec01bceb30f77961eb1ec2149a4405ad0826c47a
papabn <pndiaye999@gmail.com>
2025-10-05 20:49:26 +0000

    feat: Create student form and set up routing for student management

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateStudent } from './Students/create-student/create-student';
+import { IndexStudent } from './Students/index-student/index-student';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+// route pour la page d'accueil
+    path: '',
+    component: IndexStudent,
+  },
+  {
+    // ! route pour la création d'un étudiant
+    path: 'create-student',
+    component: CreateStudent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
